ec9d38a731146182b2b89d7f6b2a9764003281b0
KayyMoreira <kailanemoreira020@gmail.com>
2024-12-03 21:50:32 -0300

    javascript

---
+++ botao.js
@@ -0,0 +1,8 @@
+let botao = document.getElementById("botao"); 
+
+botao.addEventListener("click" , function clicar() {
+
+    const p = document.getElementById("resposta");
+    p.innerHTML = "Você clicou no botão";
+}); // adiciona um evento de clique ao botão.
+   
--- condicional.js
+++ condicional.js
@@ -1,15 +1,10 @@
-let peso = 53;
-let altura = 1.65;
-   
-let imc = peso / (altura * altura);
-if (imc < 18.5) {
-    console.log("Abaixo do peso");
-} else if (imc >= 18.5 && imc < 25) {
-    console.log("Peso ideal");
-} else if (imc >= 25 && imc < 30) {
-    console.log("Sobrepeso");
-} else if (imc >= 30 && imc < 40) {
-    console.log("Obesidade");
-} else {
-    console.log("Obesidade mórbida");
-}
+let coisas = [10, "fruta", true, "goiaba"];
+
+coisas.push(15);
+
+console.log(coisas);
+
+console.log(coisas[0]);
+console.log(coisas[1]);
+console.log(coisas[2]);
+console.log(coisas[3]);
